d7ba810b565716a8b73d42a14705fffde07fcdb9
FelemixX <83292474+FelemixX@users.noreply.github.com>
2024-06-06 22:30:45 +0300

    Добавил методов для работы с абстрактную модель

--- local/php_interface/src/App/Database/Model/AbstractModel.php
+++ local/php_interface/src/App/Database/Model/AbstractModel.php
@@ -9,10 +9,21 @@ abstract class AbstractModel extends AbstractConnection
     protected object $query;
     public string $tableName;
 
-    abstract public function select(): static;
-    abstract public function insert(): static;
-    abstract public function update(): static;
-    abstract public function delete(): static;
+    public const JOIN_TYPE_INNER = 'INNER';
+    public const JOIN_TYPE_LEFT = 'LEFT';
+    public const JOIN_TYPE_OUTER = 'OUTER';
+    public const JOIN_TYPE_CROSS = 'CROSS';
+
+    abstract public function select(array $fields): static;
+    abstract public function insert(array $fields): static;
+    abstract public function update(array $fields): static;
+    abstract public function delete(array $fields): static;
+    abstract public function sort(array $fields): static;
+    abstract public function where(array $fields): static;
+    abstract public function join(string $tableName, string $ref, string $value): static;
+    abstract public function limit(int $limit): static;
+    abstract public function offset(int $offset): static;
+
     abstract public function exec(): object;
 
     /**
@@ -22,5 +33,25 @@ abstract class AbstractModel extends AbstractConnection
     public function getTableName(): string
     {
         throw new \Exception('You must override table name');
+    }
+
+    /**
+     * @param array $fields
+     * @return void
+     * @throws \Exception
+     */
+    protected function checkRequiredFields(array $fields): void
+    {
+        if (!in_array('ID', $fields)) {
+            if (!in_array('*', $fields)) {
+                throw new \Exception('ID must be selected');
+            }
+        }
+
+        foreach ($fields as $field) {
+            if (is_array($field)) {
+                throw new \Exception('Array of arrays is not supported');
+            }
+        }
     }
 }
