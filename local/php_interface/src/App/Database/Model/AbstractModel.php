f05999db3067ef84dbd000d78918c5df3b37ffc5
FelemixX <83292474+FelemixX@users.noreply.github.com>
2024-06-06 21:39:15 +0300

    Добавил геттер названия таблицы. Добавил классу поле для названия таблицы

--- local/php_interface/src/App/Database/Model/AbstractModel.php
+++ local/php_interface/src/App/Database/Model/AbstractModel.php
@@ -7,10 +7,20 @@ use App\Database\Connection\AbstractConnection;
 abstract class AbstractModel extends AbstractConnection
 {
     protected object $query;
+    public string $tableName;
 
     abstract public function select(): static;
     abstract public function insert(): static;
     abstract public function update(): static;
     abstract public function delete(): static;
     abstract public function exec(): object;
+
+    /**
+     * @return string
+     * @throws \Exception
+     */
+    public function getTableName(): string
+    {
+        throw new \Exception('You must override table name');
+    }
 }
