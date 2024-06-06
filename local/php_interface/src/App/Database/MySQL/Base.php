28d04a36447d7955e9075e5e2c005c646652f840
FelemixX <83292474+FelemixX@users.noreply.github.com>
2024-06-06 21:25:47 +0300

    Убрал todo из логирования

--- local/php_interface/src/App/Database/MySQL/Base.php
+++ local/php_interface/src/App/Database/MySQL/Base.php
@@ -41,7 +41,7 @@ abstract class Base extends AbstractModel
     private function handleConnectionError(\Throwable $exception): never
     {
         $curDate = date('Y-m-d');
-        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/local/log/database/db_error_$curDate.log", "\n" . date('d-m-Y H:i:s', time()) . ' ' . __FILE__ . ':' . __LINE__ . ' : ' . "\n" . var_export($exception->getMessage(),true) . "\n-------------------\n", FILE_APPEND); //TODO: DELETE LOGGING
+        file_put_contents($_SERVER["DOCUMENT_ROOT"] . "/local/log/database/db_error_$curDate.log", "\n" . date('d-m-Y H:i:s', time()) . ' ' . __FILE__ . ':' . __LINE__ . ' : ' . "\n" . var_export($exception->getMessage(),true) . "\n-------------------\n", FILE_APPEND);
         die('Something went wrong with the database connection');
     }
 }
