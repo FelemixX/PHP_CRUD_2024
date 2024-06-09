ad8e07794834be5245335878eb2d070ad519a475
FelemixX <83292474+FelemixX@users.noreply.github.com>
2024-06-09 16:18:15 +0300

    Добавил страницу промежуточной таблицы товар-клиент

---
+++ client-product/actions/create.php
@@ -0,0 +1,43 @@
+<?php
+
+require_once($_SERVER['DOCUMENT_ROOT'] . '/local/php_interface/lib/vendor/autoload.php');
+
+use App\Controller\CreateController;
+use App\Database\MySQL\Models\ProductModel;
+use App\Response\Interface\ResponseTypesInterface;
+use App\Response\ResponseFactory;
+
+$response = ResponseFactory::create(ResponseTypesInterface::JSON);
+
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+    $response->setStatusCode(400);
+    $response->setSuccess(false);
+    $response->setMessage('Request method is not supported');
+    $response->send();
+    die();
+}
+
+$input = file_get_contents('php://input');
+$data = json_decode($input);
+
+try {
+    $createController = new CreateController($data, new ProductModel());
+    $result = $createController->process();
+    if (!$result) {
+        $response->setStatusCode(409);
+        $response->setSuccess(true);
+        $response->setMessage('Creation failed');
+        $response->send();
+    }
+
+    $response->setStatusCode(201);
+    $response->setSuccess(true);
+    $response->setData(['createdRowsCount' => $result]);
+    $response->send();
+} catch (Throwable $e) {
+    $response = ResponseFactory::create(ResponseTypesInterface::JSON);
+    $response->setStatusCode(500);
+    $response->setSuccess(false);
+    $response->setMessage('Something went wrong');
+    $response->send();
+}
